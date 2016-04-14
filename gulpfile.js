a3f46d375c82ded5caadcddac715cdf068c700d3
Oleh Zasadnyy <oleh.zasadnyy@gmail.com>
2016-04-14 17:48:24 +0300

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var historyApiFallback = require('connect-history-api-fallback');
+
+gulp.task('serve', function() {
+  browserSync({
+    port: 5000,
+    notify: false,
+    logPrefix: 'Polymer codelab',
+    snippetOptions: {
+      rule: {
+        match: '<span id="browser-sync-binding"></span>',
+        fn: function(snippet) {
+          return snippet;
+        }
+      }
+    },
+    // Run as an https by uncommenting 'https: true'
+    // Note: this uses an unsigned certificate which on first access
+    //       will present a certificate warning in the browser.
+    // https: true,
+    server: {
+      baseDir: ['.tmp', 'app'],
+      middleware: [historyApiFallback()]
+    }
+  });
+
+  gulp.watch(['app/**/*'], reload);
+});
